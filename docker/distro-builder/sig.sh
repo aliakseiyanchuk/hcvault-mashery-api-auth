26961927d1bf04c1c04c05fc9895e27c755d6d99
Aliaksei Yanchuk <aliaksei.yanchuk@hotmail.com>
2024-07-20 13:53:10 +0200

    Changing to multi-platform executable.

--- docker/distro-builder/sig.sh
+++ docker/distro-builder/sig.sh
@@ -4,13 +4,18 @@ if [ "$1" = "" ] ; then
   echo "Please supply an architecture"
   exit 1
 fi
-
 FILE=$(find /home/distro/$1 -name 'hcvault-mashery-api-auth_v*.sha256' )
 
 if [ "$FILE" = "" ] ; then
-  echo "No signature for architecture $1"
+  echo "No signature found"
   exit 1
 else
-  basename $FILE .sha256
-  cat $FILE | awk -F'= ' '{print $2}'
+  CMD=$(basename $FILE .sha256)
+  SHA=$(cat $FILE | awk -F'= ' '{print $2}')
+  VERSION=$(echo $FILE | sed 's/^.*_v\([0-9\.]*\).sha256$/\1/')
+
+  echo $CMD
+  echo $VERSION
+  echo $SHA
+  echo vault plugin register -sha256=${SHA} -command=${CMD} -version=${VERSION} secret hcvault-mashery-api-auth
 fi
